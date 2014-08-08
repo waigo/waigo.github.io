8bf2100556fbd84271b7ca4646dde291a8710a9c
Ramesh Nair <ram@hiddentao.com>
2014-08-09 00:37:11 +0800

    Text updates

--- build/lib/sites.js
+++ build/lib/sites.js
@@ -1,10 +1,5 @@
 module.exports = [
   {
-    url: 'https://autonomail.com',
-    title: 'Autonomail.com',
-    body: 'Autonomail is a secure email service with PGP encryption.'
-  },
-  {
     url: 'https://github.com/hiddentao/melkor',
     title: 'Melkor',
     body: 'Open-source wiki engine with Git-backed storage.'
