0fd1b7008183407203ee4cb2b93d79e02568131c
Ramesh Nair <ram@hiddentao.com>
2016-08-15 23:02:48 +0800

    more homepage work

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ export default class Footer extends React.Component {
           Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
         </div>
         <div className="social">
-          <a title="Twitter" href='https://www.twitter.com/halfatheist'>
+          <a title="Twitter" href='https://www.twitter.com/waigojs'>
             <i className="twitter" />
           </a>
         </div>
