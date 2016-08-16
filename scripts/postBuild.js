c76093a0edddc8ad0ca7ada4ec09871575b0013c
Ramesh Nair <ram@hiddentao.com>
2016-08-16 15:30:43 +0800

    added v1 docs for legacy

--- scripts/postBuild.js
+++ scripts/postBuild.js
@@ -5,3 +5,4 @@ const shell = require('shelljs');
 shell.cp('CNAME', 'public');
 shell.cp('pages/img/logo.ico', 'public/favicon.ico');
 shell.cp('node_modules/fa-stylus/fonts/*', 'public/fonts');
+shell.cp('-rf', 'v1', 'public/v1');
